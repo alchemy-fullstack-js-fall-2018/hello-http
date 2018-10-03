2b371782645b23e134096952bb77ccb76caa5415
Jack <ajackofsometrades@gmail.com>
2018-10-02 17:11:56 -0700

    second test for bodyparser failing

--- test/body-parser.test.js
+++ test/body-parser.test.js
@@ -9,18 +9,28 @@ describe('bodyParser', () => {
         req.header['content-type'] = 'application/json';
 
         const promise = bodyParser(req)
-            .then(body => {
-                expect(body).toEqual({ name: 'test'})
-            });
+            .then(body => expect(body).toEqual({ name: 'test'}));
 
-        req.emit('data', JSON.stringify({
-            name: 'test',
-        }))
-
+        req.emit('data', JSON.stringify({ name: 'test' }));
         req.emit('end');
 
         return promise;
+    });
 
+    it('fails gracefully if not json', () => {
+
+        let req = new EventEmitter();
+        req.header = {};
+        req.header['content-type'] = 'text/html';
+
+        const promise = bodyParser(req)
+            .then(() => expect(false))
+            .catch(err => expect(err).toBeTruthy());
+
+        req.emit('data', '<html></html>')
+        req.emit('end');
+
+        return promise;
     });
 
 
