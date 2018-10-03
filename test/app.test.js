cd20c4466772a24a2976f0296f328419d349324f
KLPainter <KLPainter@gmail.com>
2018-10-03 09:46:06 -0700

    three happy birthday tests passing

--- test/app.test.js
+++ test/app.test.js
@@ -9,6 +9,20 @@ describe('simple http server', () => {
         });
     });
 
+    it('responds with happy birthday plus extra message', () => {
+        const expected = '<html><body><p>Happy Birthday <strong>Jane!</strong> You Rock</p></body></html>';
+        return request(app).get('/happy-birthday/Jane?custom=You%20Rock').then(res => {
+            expect(res.text).toEqual(expected);
+        });
+    });
+
+    it('responds with Stranger if no name is provided', () => {
+        const expected = '<html><body><p>Happy Birthday <strong>Stranger!</strong> You Rock</p></body></html>';
+        return request(app).get('/happy-birthday?custom=You%20Rock').then(res => {
+            expect(res.text).toEqual(expected);
+        });
+    });
+
     it.skip('responds with hi there when path is /hello', () => {
         return request(app).get('/hello')
             .then(res => {
