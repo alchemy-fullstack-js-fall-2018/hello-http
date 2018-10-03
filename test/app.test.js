cae1679bab7384608234f0a6e11fdb55ba854ae6
KLPainter <KLPainter@gmail.com>
2018-10-02 17:52:25 -0700

    one test passing

--- test/app.test.js
+++ test/app.test.js
@@ -1,19 +1,18 @@
 const request = require('supertest');
-
-// import our server app
 const app = require('../lib/app');
 
 describe('simple http server', () => {
-    it('responds with hello world on GET', () => {
-        return request(app).get('/').then(res => {
-            expect(res.text).toEqual('hello world');
+    it('responds with happy birthday', () => {
+        const expected = '<html><body><p>Happy Birthday <strong>Elmo!</strong></p></body></html>';
+        return request(app).get('/happy-birthday/Elmo').then(res => {
+            expect(res.text).toEqual(expected);
         });
     });
 
-    it('responds with hi there when path is /hello', () => {
+    it.skip('responds with hi there when path is /hello', () => {
         return request(app).get('/hello')
             .then(res => {
                 expect(res.text).toEqual('hi there');
             });
-    })
+    });
 });
