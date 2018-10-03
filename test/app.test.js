f5873c065650a866285f3f364ddc3c6bf855fd51
Jack <ajackofsometrades@gmail.com>
2018-10-02 18:18:23 -0700

    receiving random fact at /fact

--- test/app.test.js
+++ test/app.test.js
@@ -3,25 +3,38 @@ const app = require('../lib/app');
 
 describe('hello http server', () => {
 
-    it('first test', () => {
-        return request(app).get('/').then(res => {
-            expect(res.text).toEqual('hello world');
+    describe('greetings', () => {
+    
+        it('Respond with a greeting if GET and url /happy-birthday/<name>', () => {
+            return request(app).get('/happy-birthday/GI-Joe')
+                .then(res => {
+                    expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>GI-Joe!</strong></p></body></html>');
+                });
+        });
+        it('Respond with Stranger if GET and url /happy-birthday/', () => {
+            return request(app).get('/happy-birthday/')
+                .then(res => {
+                    expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>Stranger!</strong></p></body></html>');
+                });
+        });
+        it('Adds custom greeting if provided in query string', () => {
+            return request(app).get('/happy-birthday/GI-Joe?custom=Go%20Joe')
+                .then(res => {
+                    expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>GI-Joe!</strong> Go Joe</p></body></html>');
+                });
         });
     });
-    it('Respond with a greeting if GET and url /happy-birthday/<name>', () => {
-        return request(app).get('/happy-birthday/GI-Joe').then(res => {
-            expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>GI-Joe!</strong></p></body></html>');
+
+    describe('facts', () => {
+
+        it('Responds with random fact if GET and url /fact', () => {
+            return request(app).get('/fact')
+                .then(res => {
+                    expect(res.text).toContain('http');
+                });
         });
-    });
-    it('Respond with Stranger if GET and url /happy-birthday/', () => {
-        return request(app).get('/happy-birthday/').then(res => {
-            expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>Stranger!</strong></p></body></html>');
-        });
-    });
-    it('Adds custom greeting if provided in query string', () => {
-        return request(app).get('/happy-birthday/GI-Joe?custom=Go%20Joe').then(res => {
-            expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>GI-Joe!</strong> Go Joe</p></body></html>');
-        });
+
+
     });
 
 
