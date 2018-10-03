f7c1a5647f2c162782fd4a0448b5e4baef9503ad
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-10-02 17:34:30 -0700

    custom input working

--- test/app.test.js
+++ test/app.test.js
@@ -13,7 +13,25 @@ describe('simple http server', () => {
     it('responds Happy Birthday Mack', () => {
         return request(app).get('/happy-birthday/Mack')
             .then(res => {
-                expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>Mack!</strong></p></body></html>');
+                expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>Mack!</strong> </p></body></html>');
+            });
+    });
+    it('responds Happy Birthday Stranger when no name is given', () => {
+        return request(app).get('/happy-birthday')
+            .then(res => {
+                expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>Stranger!</strong> </p></body></html>');
+            });
+    });
+    it('responds Happy Birthday Mack with custom input', () => {
+        return request(app).get('/happy-birthday/Mack?custom=Hi')
+            .then(res => {
+                expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>Mack!</strong> Hi</p></body></html>');
+            });
+    });
+    it('gives random http fact', () => {
+        return request(app).get('/fact')
+            .then(res => {
+                expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>Mack!</strong> Hi</p></body></html>');
             });
     });
 });
