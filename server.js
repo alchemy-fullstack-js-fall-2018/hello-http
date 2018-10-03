0ae1dd4ca7511fac69363ba52a732cec818d945b
Ryan Luras <ryanluras@gmail.com>
2018-10-03 09:18:29 -0700

    adding classwork files as starting point to see things work

---
+++ server.js
@@ -0,0 +1,14 @@
+/* eslint no-console: off */
+
+const app = require('app');
+const http = require('http');
+
+//keeping this momentarily till the weekend for refrence
+// curl 'http://localhost:7890/hi' -d '{ "name": "ryan" }'
+const server = http.createServer(app);
+
+const PORT = 7890;
+
+server.listen(PORT, () => {
+    console.log('server running on', server.address().port);
+});
