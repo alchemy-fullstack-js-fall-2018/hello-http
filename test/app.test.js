05fc8e833205c8f927a9cf3d044a12f12ff51b77
Jack <ajackofsometrades@gmail.com>
2018-10-02 17:43:33 -0700

    test #2 is failing

--- test/app.test.js
+++ test/app.test.js
@@ -4,14 +4,14 @@ const app = require('../lib/app');
 describe('hello http server', () => {
 
     it('first test', () => {
-
         return request(app).get('/').then(res => {
-
             expect(res.text).toEqual('hello world');
-
         });
-
-
+    });
+    it('Respond with a greeting if GET and url /happy-birthday/<name>', () => {
+        return request(app).get('/happy-birthday/GI-Joe').then(res => {
+            expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>GI-Joe!</strong></p></body></html>');
+        });
     });
 
 
