e35d51e6b025b8e6fdfba2976e79f85c8d0de849
Sophie Graham <sophiergraham1@gmail.com>
2018-10-02 17:39:42 -0700

    passed first test

--- Test/app.test.js
+++ Test/app.test.js
@@ -1,9 +1,11 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('', () => {
-    it('', () => {
-
+describe('simple http server', () => {
+    it('responds with greeting on GET', () => {
+        return request(app).get('/').then(res => {
+            expect(res.text).toEqual('hello world');
+        });
     });
 });
 
