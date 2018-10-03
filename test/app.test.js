66648510e7739dd9c6bf307e06c0facc9a2a02b3
MikeBLambert <mlamb128@gmail.com>
2018-10-03 11:36:07 -0700

    Small fix'

--- test/app.test.js
+++ test/app.test.js
@@ -10,38 +10,41 @@ describe('simple http server', () => {
             });
     });
 
-    it('responds with <html><body><p>Happy Birthday <strong>USER</strong></p></body></html> if method is GET and url is /happy-birthday/USER', () => {
-        return request(app).get('/happy-birthday/John')
-            .then(res => {
-                expect(res.text).toEqual(
-                    '<html><body><p>Happy Birthday <strong>John!</strong></p></body></html>'
-                );
-            });
-    });
+    describe('happy birthday', () => {
 
-    it('replaces name with "Stranger" if there is no name supplied', () => {
-        return request(app).get('/happy-birthday/')
-            .then(res => {
-                expect(res.text).toEqual(
-                    '<html><body><p>Happy Birthday <strong>Stranger!</strong></p></body></html>'
-                );
-            });
-    });
-
-    it('gives a separate response if GET request does not start with /happy-birthday/', () => {
-        return request(app).get('/happy-anniversary')
-            .then(res => {
-                expect(res.text).toEqual('Sorry, we only celebrate birthdays and facts here...sad panda ;(');
-            });
-    });
-
-    it('adds custom response for query strings', () => {
-        return request(app).get('/happy-birthday/Jane&custom=You%20Rock')
-            .then(res => {
-                expect(res.text).toEqual(
-                    '<html><body><p>Happy Birthday <strong>Jane!</strong> You Rock</p></body></html>'
-                );
-            });
+        it('responds with <html><body><p>Happy Birthday <strong>USER</strong></p></body></html> if method is GET and url is /happy-birthday/USER', () => {
+            return request(app).get('/happy-birthday/John')
+                .then(res => {
+                    expect(res.text).toEqual(
+                        '<html><body><p>Happy Birthday <strong>John!</strong></p></body></html>'
+                    );
+                });
+        });
+    
+        it('replaces name with "Stranger" if there is no name supplied', () => {
+            return request(app).get('/happy-birthday/')
+                .then(res => {
+                    expect(res.text).toEqual(
+                        '<html><body><p>Happy Birthday <strong>Stranger!</strong></p></body></html>'
+                    );
+                });
+        });
+    
+        it('gives a separate response if GET request does not start with /happy-birthday/', () => {
+            return request(app).get('/happy-anniversary')
+                .then(res => {
+                    expect(res.text).toEqual('Sorry, we only celebrate birthdays and facts here...sad panda ;(');
+                });
+        });
+    
+        it('adds custom response for query strings', () => {
+            return request(app).get('/happy-birthday/Jane&custom=You%20Rock')
+                .then(res => {
+                    expect(res.text).toEqual(
+                        '<html><body><p>Happy Birthday <strong>Jane!</strong> You Rock</p></body></html>'
+                    );
+                });
+        });
     });
 
     describe('facts', () => {
