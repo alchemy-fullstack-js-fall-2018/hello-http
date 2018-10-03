cd003187bc025c07e70c669365f27975831cdf9e
Jack <ajackofsometrades@gmail.com>
2018-10-02 17:53:57 -0700

    failing on custom greeting

--- test/app.test.js
+++ test/app.test.js
@@ -13,6 +13,12 @@ describe('hello http server', () => {
             expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>GI-Joe!</strong></p></body></html>');
         });
     });
+    it('Adds custom greeting if provided in query string', () => {
+
+        return request(app).get('/happy-birthday/GI-Joe&custom=Go%20Joe').then(res => {
+            expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>GI-Joe!</strong> Go Joe</p></body></html>');
+        });
+    });
 
 
 });
