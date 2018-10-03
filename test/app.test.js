cb7d043db84fa8def4efdc2359c618ef9ec805cc
Alex Rankin <alex.c.rankin@gmail.com>
2018-10-02 17:05:05 -0700

    saying happy birthday to myself

--- test/app.test.js
+++ test/app.test.js
@@ -7,5 +7,18 @@ describe('my http server', () => {
             expect(res.text).toEqual('"Hello World"');
         });
     });
-    
+
+    it('responds with "hey back at you" when path is /hi', () => {
+        return request(app).get('/hi')
+            .then(res => {
+                expect(res.text).toEqual('"hey back at you"');
+            });
+    });
+
+    it('responds Happy Birthday Al when path is /happy-birthday/Al', () => {
+        return request(app).get('/happy-birthday/Al')
+            .then(res => {
+                expect(res.text).toEqual('Happy Birthday Al');
+            });
+    })
 });
