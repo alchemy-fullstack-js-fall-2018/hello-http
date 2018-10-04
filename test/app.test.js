872b59c87f9a8bccce80006ec7cf664c1ef7035c
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-03 17:57:08 -0700

    wrote failing test for happy birthday get

--- test/app.test.js
+++ test/app.test.js
@@ -3,9 +3,17 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('simple http server', () => {
-    it('responds with a happy birthday + name method', () => {
+    it('responds hello world to basic get request', () => {
         return request(app).get('/').then(res => {
             expect(res.text).toEqual('hello world');
         });
     });
+
+    it('responds with a birthday + name greeting if the method is `GET` and url (path) is `/happy-birthday/<name>`', () => {
+        return request(app).get('/happy-birthday/universe').then(res => {
+            expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>universe!</strong></p></body></html>');
+        });
+    });
+
+
 });
