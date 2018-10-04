ab4e8f6bfaa800c386da7659e0c73149552b367a
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-03 17:36:59 -0700

    server.js written

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require('./lib/app');
+const http = require('http');
+
+const server = http.createServer(app);
+
+const PORT = 7899;
+
+server.listen(PORT, () => {
+    console.log('server running on', server.address().port);
+});
