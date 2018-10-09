52130598d016a71b5e2061bb619b08af1275b980
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-09 09:40:50 -0700

    refactored app,  added 404 method and test

--- test/app.test.js
+++ test/app.test.js
@@ -3,15 +3,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('simple http server', () => {
-    it('responds hello world to basic get request', () => {
-        return request(app).get('/').then(res => {
-            expect(res.text).toEqual('hello world');
-        });
-    });
-
     it('responds with a birthday + name greeting if the method is `GET` and url (path) is `/happy-birthday/<name>`', () => {
         return request(app).get('/happy-birthday/universe').then(res => {
-            expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>universe!</strong></p></body></html>');
+            expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>universe!</strong> </p></body></html>');
         });
     });
 
@@ -23,7 +17,7 @@ describe('simple http server', () => {
 
     it('responds with a birthday + stranger greeting if no name is provided', () => {
         return request(app).get('/happy-birthday').then(res => {
-            expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>Stranger!</strong></p></body></html>');
+            expect(res.text).toEqual('<html><body><p>Happy Birthday <strong>Stranger!</strong> </p></body></html>');
         });
     });
 
@@ -31,6 +25,14 @@ describe('simple http server', () => {
         return request(app).get('/fact').then(res => {
             expect(res.text).toEqual(expect.stringMatching(/HTTP/));
         });
+    });
+
+    it('responds with 404 not found', () => {
+        return request(app).get('/ladeedah')
+            .then(res => {
+                expect(res.status).toEqual(404);
+                expect(res.text).toMatch(/CANNOT/);
+            });
     });
 
 });
